b0d69513848aa59bf14a3e08d711f7dffda33a4d
Peter Salanki <peter@salanki.st>
2021-09-24 01:38:00 -0400

    Revert "Set VirtioFS Metadata Cache to Auto"
    This is still not fixed in libvirt.
    
    This reverts commit 956aa0a594eb05be6aef8d81eb2189a3e68788ba.

--- pkg/virt-launcher/virtwrap/converter/converter.go
+++ pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1549,7 +1549,7 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 				Path:  "/usr/libexec/virtiofsd",
 				Xattr: "on",
 				Cache: &api.FilesystemBinaryCache{
-					Mode: "auto",
+					Mode: "none",
 				},
 				Lock: &api.FilesystemBinaryLock{
 					Posix: "on",
